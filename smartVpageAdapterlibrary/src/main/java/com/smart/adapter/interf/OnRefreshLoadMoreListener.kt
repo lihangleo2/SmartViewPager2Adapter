5dae2e400e8b67906d8fbdb4c7e4f6cd74e20bb7
lihang <lihangleo2@163.com>
2023-09-09 22:33:59 +0800

    SmartAdapter强势来袭

---
+++ smartVpageAdapterlibrary/src/main/java/com/smart/adapter/interf/OnRefreshLoadMoreListener.kt
@@ -0,0 +1,8 @@
+package com.smart.adapter.interf
+
+/**
+ * @Author leo
+ * @Date 2023/9/6
+ */
+interface OnRefreshLoadMoreListener:OnRefreshListener,OnLoadMoreListener {
+}
---
+++ smartVpageAdapterlibrary/src/main/java/com/smart/adapter/interf/SmartFragmentImpl.kt
@@ -0,0 +1,10 @@
+package com.smart.adapter.interf
+
+/**
+ * @Author leo
+ * @Date 2023/9/7
+ * 必须要实现此接口否则报错
+ */
+interface SmartFragmentImpl {
+    fun initSmartFragmentData(bean: SmartFragmentTypeExEntity)
+}
